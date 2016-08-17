4c2a71adc7bc238d749d44b8aae05a119d53fed0
huazontla <oswaldoag2614@gmail.com>
2016-08-17 13:33:30 -0500

    Agregando variables para una simple suma

--- holaMundo.rb
+++ holaMundo.rb
@@ -1,13 +1,15 @@
 =begin
 	En esta zona se hace la definicion de la clase
 	Hola Mundo y se colocan el metodo constructor
-	y se crea un metodo para saludar saluda()
+	y se crea un metodo para saludar saluda(d)
 =end
 class HolaMundo
 	def initialize()
 	end
 	def saluda()
-		puts "Hola Mundo" # Imprimir el mensaje Hola Mundo
+		valor1 = 1
+		valor2 = 2
+		puts valor1 + valor2 # Imprimir el resultado de la suma
 	end
 end
 # Creando el objeto de la clase HolaMundo
