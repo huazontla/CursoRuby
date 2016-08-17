26c5628acf60818030997302508535871dcec905
huazontla <oswaldoag2614@gmail.com>
2016-08-17 10:03:44 -0500

    Agregando Comentarios a la clase HolaMundo

--- holaMundo.rb
+++ holaMundo.rb
@@ -1,11 +1,16 @@
+=begin
+	En esta zona se hace la definicion de la clase
+	Hola Mundo y se colocan el metodo constructor
+	y se crea un metodo para saludar saluda()
+=end
 class HolaMundo
 	def initialize()
 	end
 	def saluda()
-		puts "Hola Mundo"
+		puts "Hola Mundo" # Imprimir el mensaje Hola Mundo
 	end
 end
-
+# Creando el objeto de la clase HolaMundo
 objeto = HolaMundo.new()
 
 objeto.saluda()
