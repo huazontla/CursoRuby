543b43e6dc149871a30a45afc3ae860fe79889b6
huazontla <oswaldoag2614@gmail.com>
2016-08-30 11:47:04 -0500

    ciclo FOR usando el do

--- holaMundo.rb
+++ holaMundo.rb
@@ -6,13 +6,14 @@ class HolaMundo
 	end
 	def saluda()
 		#cliclo for
-			for i in(1..10)
+			
+			for i in (1..20) do
 				puts i
 			end
 	end
 end
 # Creando el objeto de la clase HolaMundo
 objeto = HolaMundo.new()
-
+# Ejecutando el metodo saluda del objeto 
 objeto.saluda
 
