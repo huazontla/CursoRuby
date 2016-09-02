230c4bdadf3f1e839f2caa153ac594885042b4b4
huazontla <oswaldoag2614@gmail.com>
2016-09-02 12:18:12 -0500

    Nuevos Metodos

--- holaMundo.rb
+++ holaMundo.rb
@@ -1,15 +1,15 @@
 
 class HolaMundo
 
-	def initialize()
+	def initialize() # Metodo constructor
 		
 	end
 	def saluda()
-		#cliclo for
-			
-			for i in (1..20) do
-				puts i
-			end
+		20.times { |i| puts i} # imprimir 20 veces
+		1.upto(10) { |u| puts u} # imprimir desde el 1 al 10
+		10.downto(1) { |o| puts o} # imprimir desde el 10 al 1
+
+
 	end
 end
 # Creando el objeto de la clase HolaMundo
