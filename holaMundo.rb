70a08178478adaa3d75fcbe9b5d48d8ae3fdddca
huazontla <oswaldoag2614@gmail.com>
2016-11-07 11:58:02 -0600

    Bloques o Blocks

--- holaMundo.rb
+++ holaMundo.rb
@@ -1,38 +1,18 @@
+class Array 
 
-class HolaMundo
-
-	def initialize() # Metodo constructor
-		
-	end
-	def saluda()
-		
-		# usando Lambdas
-		# son funciones anonimas
-		# son objetos, retornan un valor
-
-		lamb = lambda {|numero| numero + 1} # se almacena el lambda en la variable lamb, en la lambda se esta esperando recibir un numero y a ese numero sumarle 1
-
-		puts lamb.call(99) # se imprimer el lambda pasando como parametro el 99 y el resultado sera 100
-
-
-		lamb2 = lambda do |nombre| # cuando se usa mas de una linea de codigo es preferente usar esta sintaxis 
-			if nombre == "Alfredo"
-				return "hola Alfredo"
-			else
-				return "hola Anonimo"
-			end		
+	def iterar
+		self.each_with_index do |n,i|
+			self[i] = yield(n)
 		end	
+	end	
 
-		puts lamb2.call("Alfredo")
-
-
-		puts "hola"
-
-
-	end
 end
-# Creando el objeto de la clase HolaMundo
-objeto = HolaMundo.new()
-# Ejecutando el metodo saluda del objeto 
-objeto.saluda
 
+arreglo = [1,2,3]
+arreglo.iterar do |n|
+		n**2
+end		
+
+for i in arreglo do
+	puts i
+end
